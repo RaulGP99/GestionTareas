bb33a77a673804b564cb82d2d53e1f090de76cbc
daw2 <daw2@A404PC04>
2023-09-25 13:32:24 +0200

    JPA

--- GestionTareas/src/main/java/org/zabalburu/GestionTareas/dao/modelo/Empleado.java
+++ GestionTareas/src/main/java/org/zabalburu/GestionTareas/dao/modelo/Empleado.java
@@ -2,6 +2,12 @@ package org.zabalburu.GestionTareas.dao.modelo;
 
 import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -11,13 +17,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "EmpleadosT")
 public class Empleado {
 	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nombre;
 	private String apellidos;
 	private String email;
 	private String password;
+	@OneToMany(mappedBy = "empleado")
 	private List<Tarea> tareas;
 	
 	@Override
--- GestionTareas/src/main/java/org/zabalburu/GestionTareas/dao/modelo/Tarea.java
+++ GestionTareas/src/main/java/org/zabalburu/GestionTareas/dao/modelo/Tarea.java
@@ -3,6 +3,13 @@ package org.zabalburu.GestionTareas.dao.modelo;
 import java.util.Date;
 import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -12,9 +19,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name="tareas")
 public class Tarea {
 	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	@ManyToOne
+	@JoinColumn(name = "idempleado")
 	private Empleado empleado;
 	private String descripcion;
 	private Date fechaInicio;
